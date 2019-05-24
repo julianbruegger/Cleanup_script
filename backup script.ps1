d13fdf6f7bc496f133bd455993b3f09ab39b072b
Julian Brügger <brj@ICT-BZ.ch>
2019-05-24 12:49:52 +0200

    Added Messagebox

--- backup script.ps1
+++ backup script.ps1
@@ -1,4 +1,14 @@
-#Backup erstellen
-Copy-Item 'G:\scanner\*' 'C:\backup\pfad\Backup'
-#Nun werden alle Daten älter als 30-Tage gelöscht
+Add-Type -AssemblyName System.Windows.Forms
+$Result = [System.Windows.Forms.MessageBox]::Show("Moechten Sie ein Backup erstellen?","Wichtig!",4)
+ 
+If ($Result -eq "Yes")
+{
+   #Backup erstellen
+   Get-ChildItem –Path "G:\scanner" -Recurse | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-30))} |Copy-Item 'G:\scanner\*' 'C:\Temp\Backup'
+}
+else
+{
+    #Nun werden alle Daten älter als 30-Tage gelöscht
 Get-ChildItem –Path "G:\scanner" -Recurse | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-30))} | Remove-Item
+}
+
