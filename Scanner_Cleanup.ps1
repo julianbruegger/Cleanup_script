1609435b4e3c37c02f808f154eb7402b3493c562
Julian Brügger <brj@ICT-BZ.ch>
2019-05-28 08:07:11 +0200

    Added a Description

--- Scanner_Cleanup.ps1
+++ Scanner_Cleanup.ps1
@@ -1,4 +1,5 @@
-# Das Script Löscht Dateien im Scanner Ordner welche älter als 30 Tage alt sind. 
+# ICT-Berufsbildung Zentralschweiz
+# Das Script löscht Dateien im Scanner Ordner welche älter als 30 Tage alt sind. 
 # V 1.0
 # Julian Brügger, 24.05.2019
 Get-ChildItem –Path "G:\scanner" -Recurse | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-30))} | Remove-Item
